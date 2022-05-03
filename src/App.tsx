2217343974f0eabd38b94863462215e7d903a50b
Danilo Caceres Tanaka <danilo.tanaka@tivit.com>
2022-05-03 18:58:41 -0300

    Include and configure tailwindcss on project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return(<h1>Hello React World!</h1>)
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? 'Default'}</button>
+}
+
+function App( ) {
+  return(
+    <div className="flex gap-2">
+      <Button text="Send"/>
+      <Button text="OK"/>
+      <Button />
+    </div>
+    )
 }
 
 export default App
